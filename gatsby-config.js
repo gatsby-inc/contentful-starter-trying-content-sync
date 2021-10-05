24ff3f07f8bc963f371733925398043ee2349f05
Tyler Barnes <tylerdbarnes@gmail.com>
2021-10-05 11:37:20 -0700

    add debug logs to build process

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,6 +2,8 @@ require('dotenv').config({
   path: `.env.${process.env.NODE_ENV}`,
 })
 
+process.env.CONTENTFUL_DEBUG_NODE_MANIFEST = `true`
+
 const contentfulConfig = {
   spaceId: process.env.CONTENTFUL_SPACE_ID,
   accessToken:
