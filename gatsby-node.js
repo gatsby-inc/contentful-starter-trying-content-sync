d805cc4780d22a2f26e3c5e6a1a4865ae7dbbb51
Tyler Barnes <tylerdbarnes@gmail.com>
2021-10-08 17:15:01 -0700

    Revert "Update gatsby-node.js"
    This reverts commit 90134948c39d5135772e2cc7bcd6216c70224417.

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,9 +1,5 @@
 const path = require('path')
 
-exports.onPreInit = ({ reporter }) => {
-  reporter.panic(`testing failed builds in content sync UI`)
-}
-
 exports.createPages = async ({ graphql, actions, reporter }) => {
   const { createPage } = actions
 
